53c0f3ccb641fe09787ec6b553d265508a72af39
zaka13 <105675209+zaka13X@users.noreply.github.com>
2026-05-29 22:20:46 -0500

    Update prxy.mjs

--- active/scripts/prxy.mjs
+++ active/scripts/prxy.mjs
@@ -28,7 +28,7 @@ export async function getUV(input) {
 
   let url = search(input, "https://html.duckduckgo.com/html?t=h_&q=%s");
 
-  let wispUrl = "wss://wisp.rhw.one/";
+  let wispUrl = "wss://wisp.mercurywork.shop/";
   if ((await connection.getTransport()) !== "/active/prxy/epoxy/index.mjs") {
     await connection.setTransport("/active/prxy/epoxy/index.mjs", [
       { wisp: wispUrl },
